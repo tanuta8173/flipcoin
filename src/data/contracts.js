e30c941a66c738bd8da4161e875db7df0c669788
trashertravis <trashertravis@gmail.com>
2023-09-19 14:16:41 +0530

    contracts updated

--- src/data/contracts.js
+++ src/data/contracts.js
@@ -1,2 +1,2 @@
 export const FLIP_CONTRACT = '0xfc473a32Bc648C4c41d9b74ead4C5B969bCfcC34' // Token Contract
-export const GAME_CONTRACT = '0x9E8CdBc59c7B28A66f26f5E62516979bdca6c23c' // Game Contract
+export const GAME_CONTRACT = '0x4021F329581beC27FfA14553DfA4d5691605Fdd1' // Game Contract
